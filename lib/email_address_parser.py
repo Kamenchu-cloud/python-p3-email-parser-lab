cd6b03e270f0df59dc3ad37260f8cdc5ab357f3a
Kamenchu <iankamenchu@gmail.com>
2024-03-10 19:04:58 +0300

    completed tests

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,14 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+
+    def parse(self):
+        # Split the string into a list of email addresses
+        addresses_list = re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', self.email_addresses)
+
+        # Remove duplicate email addresses and sort alphabetically
+        unique_addresses = sorted(set(addresses_list))
+
+        return unique_addresses
